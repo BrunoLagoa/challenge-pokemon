bba1086c979608b20a963156b42dc7e433831fc6
Bruno Castro <miroldols@gmail.com>
2023-01-17 10:25:22 -0300

    feat(routes): Implemented page home

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
+import HomePage from './pages/Home';
 import MapPage from './pages/Map';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/map" component={MapPage} />
+        <Route exact path="/map" component={MapPage} />
+        <Route path="/*" component={HomePage} />
       </Switch>
     </BrowserRouter>
   );

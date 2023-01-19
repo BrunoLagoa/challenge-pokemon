46dd44962c7a10416350e153c2c00483fcd360c0
Bruno Castro <miroldols@gmail.com>
2023-01-19 01:42:10 -0300

    fix: Updated mock pokemon

--- src/utils/mocks/pokemon.ts
+++ src/utils/mocks/pokemon.ts
@@ -3,18 +3,87 @@ import { IPokemon } from '../interfaces/IPokemon';
 const pokemon: IPokemon = {
   name: 'Pikachu',
   id: 25,
-  types: ['electric'],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: 'electric',
+        url: 'https://pokeapi.co/api/v2/type/13/',
+      },
+    },
+  ],
   height: 0.4,
   weight: 6.0,
-  abilities: ['static', 'lightning-rod'],
-  stats: {
-    hp: 35,
-    attack: 55,
-    defense: 30,
-    specialAttack: 50,
-    specialDefense: 40,
-    speed: 90,
-  },
+  abilities: [
+    {
+      ability: {
+        name: 'own-tempo',
+        url: 'https://pokeapi.co/api/v2/ability/20/',
+      },
+    },
+    {
+      ability: {
+        name: 'tangled-feet',
+        url: 'https://pokeapi.co/api/v2/ability/77/',
+      },
+    },
+    {
+      ability: {
+        name: 'contrary',
+        url: 'https://pokeapi.co/api/v2/ability/126/',
+      },
+    },
+  ],
+  stats: [
+    {
+      base_stat: 60,
+      effort: 0,
+      stat: {
+        name: 'hp',
+        url: 'https://pokeapi.co/api/v2/stat/1/',
+      },
+    },
+    {
+      base_stat: 60,
+      effort: 0,
+      stat: {
+        name: 'attack',
+        url: 'https://pokeapi.co/api/v2/stat/2/',
+      },
+    },
+    {
+      base_stat: 60,
+      effort: 0,
+      stat: {
+        name: 'defense',
+        url: 'https://pokeapi.co/api/v2/stat/3/',
+      },
+    },
+    {
+      base_stat: 60,
+      effort: 1,
+      stat: {
+        name: 'special-attack',
+        url: 'https://pokeapi.co/api/v2/stat/4/',
+      },
+    },
+    {
+      base_stat: 60,
+      effort: 0,
+      stat: {
+        name: 'special-defense',
+        url: 'https://pokeapi.co/api/v2/stat/5/',
+      },
+    },
+    {
+      base_stat: 60,
+      effort: 0,
+      stat: {
+        name: 'speed',
+        url: 'https://pokeapi.co/api/v2/stat/6/',
+      },
+    },
+  ],
   sprites: {
     other: {
       'official-artwork': {
